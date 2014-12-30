c04fff75000d12f9be43f4a05dcf2157341657f9
Ricardo Macario <ricardomacario91@gmail.com>
2014-12-29 19:40:33 -0500

    initial game controller logic

--- public/scripts/controllers/gameController.js
+++ public/scripts/controllers/gameController.js
@@ -1,28 +1,57 @@
 angular.module('app')
     .controller('gameController', function($scope){
 
+        $scope.dealer = {
+            hand: {
+                count: 14,
+                cards: [
+                    {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds'},
+                    {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds'},
+                    {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds'},
+                    {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds'}
+                ]
+            }
+        }
+
+
         $scope.players = [
             {
                 name: 'Ricardo',
                 bank: 2000,
                 hand: {count: 14, cards:
                     [
-                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds.svg'},
-                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds.svg'}
+                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds'},
+                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds'}
                     ]
-                }
+                },
+                showControls: true
             },
             {
                 name: 'Diego',
                 bank: 2000,
                 hand: {count: 14, cards:
                     [
-                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds.svg'},
-                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds.svg'}
+                        {color: 'red', suit: 'diamond', value: 7, imageName: '8_of_diamonds'},
+                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds'}
                     ]
-                }
+                },
+                showControls: false
             }
         ];
+
+        $scope.hit = function(){
+
+        }
+
+        $scope.stand = function(){
+
+        }
+        $scope.double = function(){
+
+        }
+        $scope.split = function(){
+
+        }
 
 
     })
