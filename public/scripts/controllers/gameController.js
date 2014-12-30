337d4d88c5946440d8ea658641eb7cfc1dc461b4
Ricardo Macario <ricardomacario91@gmail.com>
2014-12-29 19:20:31 -0500

    create game user mockup

--- public/scripts/controllers/gameController.js
+++ public/scripts/controllers/gameController.js
@@ -1,4 +1,28 @@
 angular.module('app')
     .controller('gameController', function($scope){
 
+        $scope.players = [
+            {
+                name: 'Ricardo',
+                bank: 2000,
+                hand: {count: 14, cards:
+                    [
+                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds.svg'},
+                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds.svg'}
+                    ]
+                }
+            },
+            {
+                name: 'Diego',
+                bank: 2000,
+                hand: {count: 14, cards:
+                    [
+                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds.svg'},
+                        {color: 'red', suit: 'diamond', value: 7, imageName: '7_of_diamonds.svg'}
+                    ]
+                }
+            }
+        ];
+
+
     })
